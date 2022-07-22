612a5bbffc678d6c102d957927cc7c5a2b38f295
Cruzxy <100546798+Cruzxy@users.noreply.github.com>
2022-07-22 17:13:36 -0300

    Add files via upload

---
+++ discordcl.lua
@@ -0,0 +1,25 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+
+src = Tunnel.getInterface("discord",src)
+
+
+Citizen.CreateThread(function()
+    while true do
+		local appID = 886013439913373698
+		local imgLogo = 'cruz'
+		local nome, sobrenome = src.Identidade()
+        local id = GetPlayerServerId(PlayerId())
+
+        SetDiscordAppId(appID)
+        SetDiscordRichPresenceAsset(imgLogo)
+        SetDiscordRichPresenceAssetText('Cruz Community Pandora')
+
+		SetDiscordRichPresenceAction(1, "Discord", "https://discord.gg/BmN9dWb9Nj")
+
+		SetRichPresence(" ID: "..id.." "..nome.." "..sobrenome.." ")
+
+        Citizen.Wait(60000)
+    end
+end)
